be4a04120d25a5d7cdbdf6858a4bb81c013354e5
Varun Tiwari <tiwarivarun819.vt33.vt@gmail.com>
2022-08-08 02:02:08 +0530

    :sparkles: Added directory/file ignoring

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,20 @@
 
 import { LOC } from './utils';
 import path from 'path';
+import minimist from 'minimist';
 
-const pathName = process.argv.slice(2)[0];
+const args = minimist(process.argv.slice(2), { '--': true });
 
-const reader = new LOC(path.resolve(pathName));
+const pathName = args['_'][0];
+const ignore = args['--'] || [];
+
+const ignoreScoped = ignore
+  .filter((p) => p.includes('/'))
+  .map((p) => path.resolve(p));
+
+const ignoreUnscoped = ignore.filter((p) => !p.includes('/'));
+
+const reader = new LOC(path.resolve(pathName), ignoreScoped, ignoreUnscoped);
 
 const numberOfLines = reader.readDirectory();
 
--- src/utils/LOC.ts
+++ src/utils/LOC.ts
@@ -1,4 +1,10 @@
-import { readdirSync, readFileSync, existsSync, statSync } from 'fs';
+import {
+  readdirSync,
+  readFileSync,
+  existsSync,
+  statSync,
+  realpathSync,
+} from 'fs';
 import { DirectoryRecurser } from '../types';
 import path from 'path';
 
@@ -6,14 +12,35 @@ export class LOC {
   public files: string[] = [];
   public result: number = 0;
 
-  constructor(public source: string) {}
+  constructor(
+    public source: string,
+    public ignoreScoped: string[],
+    public ignoreUnscoped: string[]
+  ) {}
 
   private recurseDirectory(source: string): DirectoryRecurser {
     for (const dir of readdirSync(source, { withFileTypes: true })) {
+      const nextPath = path.resolve(source, dir.name);
+
+      const arr = [nextPath];
+
+      if (this.ignoreScoped.includes(arr[0])) continue;
+
+      let skip = false;
+      this.ignoreUnscoped.forEach((p) => {
+        if (process.platform === 'win32') {
+          if (nextPath.split('\\').includes(p)) skip = true;
+        } else {
+          if (nextPath.split('/').includes(p)) skip = true;
+        }
+      });
+
+      if (skip) continue;
+
       if (dir.isDirectory()) {
-        this.recurseDirectory(path.resolve(source, dir.name));
+        this.recurseDirectory(nextPath);
       } else {
-        this.files.push(path.resolve(source, dir.name));
+        this.files.push(nextPath);
       }
     }
   }
